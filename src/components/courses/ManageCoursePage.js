dc7d1cad13d996f1c3676c095e6593178c96a291
Abu Sufian <ynor7005326@gmail.com>
2020-11-11 17:27:02 +0600

    Add update course functionality

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -59,9 +59,17 @@ ManageCoursePage.propTypes = {
     history: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course = slug && state.courses.length > 0
+    ? getCourseBySlug(state.courses, slug)
+    : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors
   };
